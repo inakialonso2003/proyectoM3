7b15b0db15d0239d4d3decb17d22c81870868c29
CapibaraLover <47402759+inakialonso2003@users.noreply.github.com>
2023-05-03 13:18:49 +0200

    Delete ProjecteCaixerAutomatic directory

--- ProjecteCaixerAutomatic/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.projectecaixerautomatic {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.projectecaixerautomatic to javafx.fxml;
-    exports com.mycompany.projectecaixerautomatic;
-}
